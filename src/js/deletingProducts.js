a547e36fe10b4a3151b52b483120114d41c2f9a3
ViacheslavTsybulskiy <126974582+v0ts@users.noreply.github.com>
2025-02-08 13:22:03 +0200

    fix deploy bug

--- src/js/deletingProducts.js
+++ src/js/deletingProducts.js
@@ -1,7 +1,6 @@
 import { deleteProductApi } from "./services/deleteProductApi"
 import { getProductsAPI } from "./services/getProductsApi"
 import {createMarkup} from "./productsLayout"
-import { deleteProduct } from "./deletingProducts";
 import { openModal } from "./modal/edit-product-modal";
 export const deleteProduct = () =>{
     const deleteBtnArr = document.querySelectorAll('.delete-btn')
